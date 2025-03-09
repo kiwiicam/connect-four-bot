9a0f72a48384bf5e716463f591895c0780ea7db7
kiwiicam <mastercamnz@gmail.com>
2025-03-10 12:20:11 +1300

    started funciton to search for all legal moves

--- connect4-bot/src/components/Board.jsx
+++ connect4-bot/src/components/Board.jsx
@@ -1,31 +1,53 @@
 import '../css/board.css';
+import React, { useEffect } from 'react';
+import { allLegalMoves } from './Bot';
 
-var currentBoard = 
-[[0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0]];
 
-function getBoard() {
-    return currentBoard;
-}
-
-function updateBoard(location = [], player) {
-    let row = location[0];
-    let col = location[1];
-    if (currentBoard[row][col] === 0) {
-        currentBoard[row][col] = player ? 'x' : 'o';
-    }
-    else {
-        console.log("illegal move");
-    }
-    console.log(currentBoard);
-
-}
 
 function Board() {
+
+    var currentBoard =
+        [[0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0]];
+
+        var currentBoard2 =
+        [[0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 'x'],
+        [0, 0, 0, 0, 0, 0, 'x'],
+        [0, 0, 0, 0, 0, 0, 'x'],
+        ['x', 0, 'x', 0, 0, 0, 'x'],
+        ['x', 0, 'x', 0, 0, 0, 'x']];
+
+
+
+
+    function getBoard() {
+        return currentBoard;
+    }
+
+    function updateBoard(location = [], player) {
+        let row = location[0];
+        let col = location[1];
+        if (currentBoard[row][col] === 0) {
+            currentBoard[row][col] = player ? 'x' : 'o';
+        }
+        else {
+            console.log("illegal move");
+        }
+        console.log(currentBoard);
+
+
+    }
+
+    useEffect(() => {
+        var result = allLegalMoves(currentBoard2);
+        console.log(result)
+    }, []);
+
     return (
         <div className="board">
             <div className='col'>
--- connect4-bot/src/components/Bot.jsx
+++ connect4-bot/src/components/Bot.jsx
@@ -1,2 +1,30 @@
 //check for all legal moves first
 //minimax algorith with Alpha-Beta Pruning ?
+
+
+export function allLegalMoves(board) {
+    //console.log(board)
+    var movesArray = []
+
+    for (var i = 0; i < board[0].length; i++) //for each column
+    {
+        for (var o = 0; o < board.length; o++) //for each row
+        {
+            if (board[o][i] === 'x') {
+                if (o > 0) {
+                    board[o - 1][i] = 'L'
+                    var result = o-1 + "," + i
+                    movesArray.push(result)
+                }
+                else {
+                    console.log("column is full")
+                }
+                break;
+            }
+            //check if entire column is empty and put legal moves at correct position
+            
+        }
+    }
+    return movesArray;
+
+}
