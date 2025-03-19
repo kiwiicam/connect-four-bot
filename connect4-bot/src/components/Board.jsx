e28b5f050abe6dc6fd6956a24036081eb16aca12
kiwiicam <mastercamnz@gmail.com>
2025-03-19 17:30:28 +1300

    close to finishing evaluation function

--- connect4-bot/src/components/Board.jsx
+++ connect4-bot/src/components/Board.jsx
@@ -1,6 +1,6 @@
 import '../css/board.css';
 import React, { useEffect } from 'react';
-import { allLegalMoves, Evaluation } from './Bot';
+import { allLegalMoves, Evaluation, checkWin } from './Bot';
 
 
 
@@ -8,17 +8,18 @@ function Board() {
 
     var currentBoard =
         [[0, 0, 0, 0, 0, 0, 0],
-        [0, 'o', 0, 0, 'x', 0, 0],
-        [0, 'o', 0, 0, 'x', 0, 0],
-        [0, 'o', 0, 0, 'x', 0, 0],
-        ['x', 'o', 0, 0, 0, 0, 0],
-        ['x', 0, 0, 0, 0, 0, 0]];
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 'o', 'o', 'o'],
+        [0, 'x', 'x', 'x', 0, 0, 0]];
+
 
     var currentBoard2 =
-        [[0, 0, 0, 0, 0, 0, 'x'],
-        [0, 0, 0, 0, 0, 'x', 0],
-        [0, 0, 0, 0, 'x', 0, 0],
-        [0, 0, 0, 'x', 0, 0, 0],
+        [[0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0]];
 
@@ -37,103 +38,6 @@ function Board() {
         }
         console.log(currentBoard);
 
-    }
-    function checkWin(board) {
-        var winnarray = []
-        var prev;
-        var count = 1;
-        //vertical win
-        for (var i = 0; i < board[0].length; i++) {
-            for (var o = 0; o < board.length; o++) {
-                var char = board[o][i]
-                if (char === 'x') {
-                    if (char === prev) {
-                        count++;
-                        if (count === 4) {
-                            console.log("WINNNER!")
-                            return;
-                        }
-                    }
-                }
-                else {
-                    count = 1;
-                }
-                prev = char;
-
-            }
-        }
-
-        //horizontal win
-        var prev;
-        var count = 1;
-        for (var i = 0; i < board.length; i++) {
-            for (var o = 0; o < board[0].length; o++) {
-                var char = board[i][o]
-                if (char === 'x') {
-                    if (char === prev) {
-                        count++;
-                        if (count === 4) {
-                            console.log("WINNNER!");
-                            return;
-                        }
-                    }
-                }
-                else {
-                    count = 1;
-                }
-                prev = char;
-
-            }
-        }
-        //TODO implement diagonal win detection
-        var prev;
-        var count = 1;
-        var two;
-        var three;
-        var four;
-        for (var i = 0; i < board.length; i++) {
-            for (var o = 0; o < board[0].length; o++) {
-                if (board[i][o] === 'x' || board[i][o] === 'o') {
-                    //left to right check
-                    if (i <= 2 && o > 2) {
-                        var prev = board[i][o];
-                        var two = board[i + 1][o - 1];
-                        var three = board[i + 2][o - 2];
-                        var four = board[i + 3][o - 3];
-
-                        console.log(prev, two, three, four);
-
-                        if (prev === two && two === three && three === four) {
-                            winnarray.push(board[i][o]);
-                            winnarray.push(board[i + 1][o - 1]);
-                            winnarray.push(board[i + 2][o - 2]);
-                            winnarray.push(board[i + 3][o - 3]);
-                            console.log("WIN BY BOTTOM-LEFT TO TOP-RIGHT DIAGONAL");
-                            console.log(winnarray);
-                            return;
-                        }
-                    }
-                    //right to left check
-                    if (i < board.length - 3 && o < board[0].length - 3) {
-                        prev = board[i][o];
-                        two = board[i + 1][o + 1]
-                        three = board[i + 2][o + 2]
-                        four = board[i + 3][o + 3]
-                        if (prev === two && two === three && three === four) {
-                            winnarray.push(board[i][o])
-                            winnarray.push(board[i + 1][o + 1])
-                            winnarray.push(board[i + 2][o + 2])
-                            winnarray.push(board[i + 3][o + 3])
-                            console.log("WIN BY DIAGONAL bottm right to top left")
-                            console.log(winnarray);
-                            return
-                        }
-                    }
-                }
-            }
-
-
-        }
     }
 
     useEffect(() => {
--- connect4-bot/src/components/Bot.jsx
+++ connect4-bot/src/components/Bot.jsx
@@ -42,6 +42,104 @@ export function allLegalMoves(board) {
     return movesArray;
 
 }
+
+export function checkWin(board) {
+    var winnarray = []
+    var prev;
+    var count = 1;
+    //vertical win
+    for (var i = 0; i < board[0].length; i++) {
+        for (var o = 0; o < board.length; o++) {
+            var char = board[o][i]
+            if (char === 'x') {
+                if (char === prev) {
+                    count++;
+                    if (count === 4) {
+                        console.log("WINNNER!")
+                        return;
+                    }
+                }
+            }
+            else {
+                count = 1;
+            }
+            prev = char;
+
+        }
+    }
+
+    //horizontal win
+    var prev;
+    var count = 1;
+    for (var i = 0; i < board.length; i++) {
+        for (var o = 0; o < board[0].length; o++) {
+            var char = board[i][o]
+            if (char === 'x') {
+                if (char === prev) {
+                    count++;
+                    if (count === 4) {
+                        console.log("WINNNER!");
+                        return;
+                    }
+                }
+            }
+            else {
+                count = 1;
+            }
+            prev = char;
+
+        }
+    }
+    //TODO implement diagonal win detection
+    var prev;
+    var count = 1;
+    var two;
+    var three;
+    var four;
+    for (var i = 0; i < board.length; i++) {
+        for (var o = 0; o < board[0].length; o++) {
+            if (board[i][o] === 'x' || board[i][o] === 'o') {
+                //left to right check
+                if (i <= 2 && o > 2) {
+                    var prev = board[i][o];
+                    var two = board[i + 1][o - 1];
+                    var three = board[i + 2][o - 2];
+                    var four = board[i + 3][o - 3];
+
+                    console.log(prev, two, three, four);
+
+                    if (prev === two && two === three && three === four) {
+                        winnarray.push(board[i][o]);
+                        winnarray.push(board[i + 1][o - 1]);
+                        winnarray.push(board[i + 2][o - 2]);
+                        winnarray.push(board[i + 3][o - 3]);
+                        console.log("WIN BY BOTTOM-LEFT TO TOP-RIGHT DIAGONAL");
+                        console.log(winnarray);
+                        return;
+                    }
+                }
+                //right to left check
+                if (i < board.length - 3 && o < board[0].length - 3) {
+                    prev = board[i][o];
+                    two = board[i + 1][o + 1]
+                    three = board[i + 2][o + 2]
+                    four = board[i + 3][o + 3]
+                    if (prev === two && two === three && three === four) {
+                        winnarray.push(board[i][o])
+                        winnarray.push(board[i + 1][o + 1])
+                        winnarray.push(board[i + 2][o + 2])
+                        winnarray.push(board[i + 3][o + 3])
+                        console.log("WIN BY DIAGONAL bottm right to top left")
+                        console.log(winnarray);
+                        return
+                    }
+                }
+            }
+        }
+
+
+    }
+}
 const evaluationTable =
     [[3, 4, 5, 7, 5, 4, 3],
     [4, 6, 8, 10, 8, 6, 4],
@@ -88,18 +186,76 @@ function evaluationInARow(board) {
         }
 
     }
+
     //check for horizontal 3 in a row taking "gravity" into account
+    console.log("func called")
     for (var i = 0; i < board.length; i++) {
         for (var o = 0; o < board[0].length; o++) {
-            
+            if (o < 4) {
+                if (board[i][o] === 'x' || board[i][o] === 'o') {
+                    player = board[i][o];
+                    if (i < 5) {
+                        if (board[i][o + 1] === player && board[i][o + 2] === player && board[i][o + 3] === 0 && board[i + 1][o + 3] !== 0) {
+                            if (player === 'x') {
+                                sum += 100
+                            }
+                            else {
+                                sum -= 100
+                            }
+                        }
+                    }
+                    else {
+                        if (board[i][o + 1] === player && board[i][o + 2] === player && board[i][o + 3] === 0) {
+                            if (player === 'x') {
+                                sum += 100
+                            }
+                            else {
+                                sum -= 100
+                            }
+                        }
+                    }
+
+                }
+            }
+            else if (o > 2) {
+                if (board[i][o] === 'x' || board[i][o] === 'o') {
+                    player = board[i][o];
+                    if (i < 5) {
+                        if (board[i][o - 1] === player && board[i][o - 2] === player && board[i][o - 3] === 0 && board[i + 1][o - 3] !== 0) {
+                            if (player === 'x') {
+                                sum += 100
+                            }
+                            else {
+                                sum -= 100
+                            }
+                        }
+                    }
+                    else {
+                        if (board[i][o - 1] === player && board[i][o - 2] === player && board[i][o - 3] === 0) {
+                            if (player === 'x') {
+                                sum += 100
+                            }
+                            else {
+                                sum -= 100
+                            }
+                        }
+                    }
+
+                }
+
+            }
 
         }
     }
 
     //check for diagonal 3 in a row, again taking "gravity" into account.
+    //left to right diagonal checking
     for (var i = 0; i < board.length; i++) {
         for (var o = 0; o < board[0].length; o++) {
+            if (i > 2 && o < 4)
+            {
 
+            }
         }
     }
     return sum;
@@ -112,7 +268,7 @@ export function Evaluation(board) {
     //check for 3 in a row for with spaces
     sum += evaluationInARow(board)
     //run the position through the evaluation table to see who has the best pieces in each position
-    //sum += evaluationTable(board)
+    //sum += evaluationTableSum(board)
 
 
     return sum;
@@ -123,9 +279,9 @@ export function Evaluation(board) {
 
 
 function minimax(board, depth, maximizingPlayer, alpha, beta) {
-    // if (depth === 0 || checkWin(board)) {
+    if (depth === 0 || checkWin(board)) {
 
-    // }
+    }
     var legalmoves = allLegalMoves(board)
 
 }
